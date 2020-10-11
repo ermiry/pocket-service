f57f73f7df92398140487be894373bb072c45236
ermiry <erick_salas@ermiry.com>
2020-10-10 22:52:00 -0500

    Added base things main methods

---
+++ include/things.h
@@ -0,0 +1,21 @@
+#ifndef _THINGS_H_
+#define _THINGS_H_
+
+#include "cerver/types/types.h"
+#include "cerver/types/string.h"
+
+#define DEFAULT_PORT					"5001"
+
+#pragma region main
+
+extern const String *PORT;
+
+// inits things main values
+extern unsigned int things_init (void);
+
+// ends things main values
+extern unsigned int things_end (void);
+
+#pragma endregion
+
+#endif
