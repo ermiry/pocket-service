5c6bf8ca780c8a008292020aac5a01d3a675fe88
ermiry <erick_salas@ermiry.com>
2020-11-09 10:27:55 -0600

    Updated version header date

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define POCKET_VERSION                    	"0.1"
 #define POCKET_VERSION_NAME               	"Version 0.1"
-#define POCKET_VERSION_DATE			    	"10/10/2020"
-#define POCKET_VERSION_TIME			    	"22:32 CST"
+#define POCKET_VERSION_DATE			    	"09/11/2020"
+#define POCKET_VERSION_TIME			    	"10:26 CST"
 #define POCKET_VERSION_AUTHOR			    "Erick Salas"
 
 // print full pocket version information
