c6e21fcd45b90b7344fe76d51d820a10ce41f19a
ermiry <erick_salas@ermiry.com>
2020-11-12 19:19:23 -0600

    Changed version methods to use cerver log

--- src/version.c
+++ src/version.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
+#include <cerver/utils/log.h>
 
 #include "version.h"
 
 // print full pocket version information
 void pocket_version_print_full (void) {
 
-	printf ("\n\nTiny Pocket Version: %s\n", POCKET_VERSION_NAME);
-	printf ("Release Date & time: %s - %s\n", POCKET_VERSION_DATE, POCKET_VERSION_TIME);
-	printf ("Author: %s\n\n", POCKET_VERSION_AUTHOR);
+	cerver_log_msg ("\n\nTiny Pocket Version: %s\n", POCKET_VERSION_NAME);
+	cerver_log_msg ("Release Date & time: %s - %s\n", POCKET_VERSION_DATE, POCKET_VERSION_TIME);
+	cerver_log_msg ("Author: %s\n\n", POCKET_VERSION_AUTHOR);
 
 }
 
 // print the version id
 void pocket_version_print_version_id (void) {
 
-	printf ("\n\nTiny Pocket Version ID: %s\n", POCKET_VERSION);
+	cerver_log_msg ("\n\nTiny Pocket Version ID: %s\n", POCKET_VERSION);
 
 }
 
 // print the version name
 void pocket_version_print_version_name (void) {
 
-	printf ("\n\nTiny Pocket Version: %s\n", POCKET_VERSION_NAME);
+	cerver_log_msg ("\n\nTiny Pocket Version: %s\n", POCKET_VERSION_NAME);
 
 }
