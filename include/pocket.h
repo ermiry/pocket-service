44374b7cacd6fe4d716a62b53aaae2f1bc9138a7
ermiry <erick_salas@ermiry.com>
2020-11-30 14:15:18 -0600

    Moved transactions api routes to dedicated sources

--- include/pocket.h
+++ include/pocket.h
@@ -69,41 +69,6 @@ extern void pocket_auth_handler (
 	const struct _HttpRequest *request
 );
 
-// GET /api/pocket/transactions
-// get all the authenticated user's transactions
-extern void pocket_transactions_handler (
-    const struct _HttpReceive *http_receive,
-	const struct _HttpRequest *request
-);
-
-// POST /api/pocket/transactions
-// a user has requested to create a new transaction
-extern void pocket_transaction_create_handler (
-    const struct _HttpReceive *http_receive,
-	const struct _HttpRequest *request
-);
-
-// GET /api/pocket/transactions/:id
-// returns information about an existing transaction that belongs to a user
-extern void pocket_transaction_get_handler (
-    const struct _HttpReceive *http_receive,
-	const struct _HttpRequest *request
-);
-
-// POST /api/pocket/transactions/:id
-// a user wants to update an existing transaction
-extern void pocket_transaction_update_handler (
-    const struct _HttpReceive *http_receive,
-	const struct _HttpRequest *request
-);
-
-// DELETE /api/pocket/transactions/:id
-// deletes an existing user's transaction
-extern void pocket_transaction_delete_handler (
-    const struct _HttpReceive *http_receive,
-	const struct _HttpRequest *request
-);
-
 #pragma endregion
 
 #endif
