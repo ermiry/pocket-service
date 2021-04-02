886d0b42b2104a018ef9dda086e2b7d32629708a
ermiry <erick.salas@ermiry.com>
2021-04-01 23:12:58 -0600

    Changed version logging methods

--- src/version.c
+++ src/version.c
@@ -5,22 +5,39 @@
 // print full pocket version information
 void pocket_version_print_full (void) {
 
-	cerver_log_msg ("\nTiny Pocket Version: %s\n", POCKET_VERSION_NAME);
-	cerver_log_msg ("Release Date & time: %s - %s\n", POCKET_VERSION_DATE, POCKET_VERSION_TIME);
-	cerver_log_msg ("Author: %s\n", POCKET_VERSION_AUTHOR);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Tiny Pocket Version: %s", POCKET_VERSION_NAME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Release Date & time: %s - %s", POCKET_VERSION_DATE, POCKET_VERSION_TIME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Author: %s\n", POCKET_VERSION_AUTHOR
+	);
 
 }
 
 // print the version id
 void pocket_version_print_version_id (void) {
 
-	cerver_log_msg ("\nTiny Pocket Version ID: %s\n", POCKET_VERSION);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Tiny Pocket Version ID: %s", POCKET_VERSION
+	);
 
 }
 
 // print the version name
 void pocket_version_print_version_name (void) {
 
-	cerver_log_msg ("\nTiny Pocket Version: %s\n", POCKET_VERSION_NAME);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Tiny Pocket Version: %s", POCKET_VERSION_NAME
+	);
 
 }
